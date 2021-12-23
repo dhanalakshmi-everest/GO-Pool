ee57c272a7a4fce474bd9b2ad1d9379bd641d478
Dhanalakshmi N <dhanalakshmi.narala@everest.engineering>
2021-12-23 12:06:33 +0530

    Add go routines pool

---
+++ pool.go
@@ -0,0 +1,41 @@
+package Go_Pool
+
+import (
+	"sync"
+)
+
+type Pool struct {
+	Tasks       []*Task
+	concurrency int
+	tasksChan   chan *Task
+	wg          sync.WaitGroup
+}
+
+func NewPool(tasks []*Task, concurrency int) *Pool {
+	return &Pool{
+		Tasks:       tasks,
+		concurrency: concurrency,
+		tasksChan:   make(chan *Task),
+	}
+}
+
+func (p *Pool) Run() {
+	for i := 0; i < p.concurrency; i++ {
+		go p.work()
+	}
+
+	p.wg.Add(len(p.Tasks))
+	for _, task := range p.Tasks {
+		p.tasksChan <- task
+	}
+
+	close(p.tasksChan)
+
+	p.wg.Wait()
+}
+
+func (p *Pool) work() {
+	for task := range p.tasksChan {
+		task.Run(&p.wg)
+	}
+}
